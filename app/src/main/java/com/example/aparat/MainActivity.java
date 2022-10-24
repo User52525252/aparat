b96f356a53e3e7a92d97ca07f0796b9ef282fb48
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-10-24 13:18:09 +0200

    rotator

--- app/src/main/java/com/example/aparat/MainActivity.java
+++ app/src/main/java/com/example/aparat/MainActivity.java
@@ -13,11 +13,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button btnTakePhoto;
     ImageView imageView;
     public static final int RequestPermissionCode = 1;
+    private int seekR, seekG, seekB, seekA;
+
+    private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            switch (seekBar.getId()) {
+                case R.id.seekBar1:
+                    seekR = progress;
+                    break;
+                case R.id.seekBar2:
+                    seekG = progress;
+                    break;
+                case R.id.seekBar3:
+                    seekB = progress;
+                    break;
+                case R.id.seekBar4:
+                    seekA = progress;
+            }
+
+            rgb();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +64,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 7);
             }
         });
+
+        SeekBar sbR = (SeekBar) findViewById(R.id.seekBar1);
+        SeekBar sbG = (SeekBar) findViewById(R.id.seekBar2);
+        SeekBar sbB = (SeekBar) findViewById(R.id.seekBar3);
+        SeekBar sbA = (SeekBar) findViewById(R.id.seekBar4);
+
+
+        sbR.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        sbG.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        sbB.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        sbA.setOnSeekBarChangeListener(onSeekBarChangeListener);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 7 && resultCode == RESULT_OK) {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
+
         }
     }
     public void EnableRuntimePermission(){
@@ -64,9 +109,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void rgb(View view) {
+    public void rgb() {
         imageView = findViewById(R.id.imageView);
-        imageView.setColorFilter(Color.argb(155, 255, 155, 155));
+        imageView.setColorFilter(Color.argb(seekA, seekR, seekG, seekB));
+
+    }
+
+    public void rotate(View view) {
+        imageView = findViewById(R.id.imageView);
+        imageView.setRotation(imageView.getRotation() + 90);
 
     }
 }
